5bc1097ac9143f94f6c1cb79a537b95a227b65aa
My Name <myEmail@example.com>
2021-05-05 21:33:49 +0700

    commit_3

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const out = document.querySelector(".out");
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", inner);
-function inner() {
-  setTimeout(function() {
-    out.innerHTML = 123;
-  }, 2000);
+const requestUrl = "https://jsonplaceholder.typicode.com/users";
+function senRequest(method, url, body = null) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+    xhr.onerror = () => {
+      console.log(xhr.response);
+    };
+    xhr.send(JSON.stringify(body));
+  });
 }
+
+// senRequest("GET", requestUrl)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+const body = {
+  name: "Toly",
+  age: 41
+};
+senRequest("POST", requestUrl, body)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
